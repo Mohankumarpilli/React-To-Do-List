a4c7c7b6f3deee85fbcfdaf1fd72dac9ec5b84a6
Mohankumarpilli <mohankumarpilli15@gmail.com>
2025-03-22 10:20:23 +0530

    done changes to fetching id

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,23 @@ import "./App.css";
 import { AddTask } from "./components/AddTask";
 
 function App() {
-  const handleEdit = (e) => {
-    const id = e.target.parentElement.parentElement.id;
+  const handleEdit = (id) => {
     const input = prompt(`edit is clicked from "${tasks[id]} "`);
     setTasks((prev) => {
-        return prev.map( (ele,index) => {
-            if(index == id){
-                return input;
-            }else{
-                return ele;
-            }
-        })
+      return prev.map((ele, index) => {
+        if (index == id) {
+          return input;
+        } else {
+          return ele;
+        }
+      });
     });
   };
 
-  const handleDelete = (e) => {
-    const id = e.target.parentElement.parentElement.id;
-    setTasks( (prev) => {
-        return prev.filter( (task,index) => index!=id );
-    })
+  const handleDelete = (id) => {
+    setTasks((prev) => {
+      return prev.filter((task, index) => index != id);
+    });
   };
   let [text, setText] = useState("");
   let [tasks, setTasks] = useState([]);
@@ -49,13 +47,27 @@ function App() {
       <div>
         <h1 className="text-7xl p-20 font-bold text-white">To-Do-List</h1>
         <form onSubmit={handleClick} className="flex pl-23">
-          <input onChange={handleChange} type="text" value={text} className="h-10 bg-white rounded-l-2xl" />
-          <button onClick={handleClick} className="h-10 w-30 bg-blue-600 rounded-r-2xl font-bold" >Add-task</button>
+          <input
+            onChange={handleChange}
+            type="text"
+            value={text}
+            className="h-10 bg-white rounded-l-2xl"
+          />
+          <button
+            onClick={handleClick}
+            className="h-10 w-30 bg-blue-600 rounded-r-2xl font-bold"
+          >
+            Add-task
+          </button>
         </form>
       </div>
       <div>
         <ul className="flex flex-col justify-center items-center list-none">
-          <AddTask tasks={tasks} handleEdit = {handleEdit} handleDelete = {handleDelete} />
+          <AddTask
+            tasks={tasks}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+          />
         </ul>
       </div>
     </>
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -6,8 +6,8 @@ export const AddTask = ({ tasks, handleEdit, handleDelete }) => {
         return <li key={index} id={index} className="flex justify-between bg-white rounded-2xl h-10 items-center pl-4 pr-4 m-5">  
           <p className="font-bold">{ele}</p>
           <div className="flex flex-row gap-5 ml-5">
-            <button className="bg-green-700 w-15 text-white rounded-2xl" onClick={(e) => handleEdit(e)}>edit</button>
-            <button className="bg-red-700 w-15 text-white rounded-2xl" onClick={(e) => handleDelete(e)}>delete</button>
+            <button className="bg-green-700 w-15 text-white rounded-2xl" onClick={() => handleEdit(index)}>edit</button>
+            <button className="bg-red-700 w-15 text-white rounded-2xl" onClick={() => handleDelete(index)}>delete</button>
           </div>
         </li>
       })}
